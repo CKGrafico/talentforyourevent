76a4c73de480b182f24db18c40752f14a261f3a1
Quique Fdez Guerra <quique@ckgrafico.com>
2022-04-04 18:26:06 +0300

    Fix dependencies

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,14 +1,13 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt3';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   publicRuntimeConfig: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    ISSUES_SSE_URL: process.env.ISSUES_SSE_URL,
+    DATABASE_URL: process.env.DATABASE_URL
   },
-  buildModules: ['@unocss/nuxt', '@vueuse/core/nuxt'],
-  unocss: {
-    preflight: true,
-    icons: true,
-  },
-})
+  buildModules: ['@vueuse/nuxt', '@pinia/nuxt'],
+  vueuse: {
+    ssrHandlers: true
+  }
+});
