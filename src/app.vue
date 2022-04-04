703cd0dbab02729bfea49173a6dcbe9725add6ee
Quique Fdez Guerra <quique@ckgrafico.com>
2022-04-04 19:38:29 +0300

    Clean classes from example

--- src/app.vue
+++ src/app.vue
@@ -17,10 +17,7 @@ useMeta({
 
 <template>
   <AppContainer>
-    <h1 class="text-4xl flex items-center gap-2">
-      Discuss with Atinux <i class="inline-flex i-carbon-chat text-black" />
-    </h1>
     <User v-if="user" :user="user" />
-    <button v-else @click="login" class="border rounded px-2 py-1">Login with GitHub</button>
+    <button v-else @click="login">Login with GitHub</button>
   </AppContainer>
 </template>
