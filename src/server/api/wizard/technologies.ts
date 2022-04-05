a362717ed50e99416c744f947102fc77875acebf
Quique Fdez Guerra <quique@ckgrafico.com>
2022-04-05 21:31:22 +0300

    Add api tech

--- src/server/api/wizard/technologies.ts
+++ src/server/api/wizard/technologies.ts
@@ -1,9 +1,20 @@
 import prismaClient from '@prisma/client';
+import { useQuery } from 'h3';
 const { PrismaClient } = prismaClient;
 
 export default async (req, res) => {
+  const query = useQuery(req);
+
+  if (!query.category) {
+    return [];
+  }
+
   const prisma = new PrismaClient();
-  const technologies = await prisma.technology.findMany();
+  const technologies = await prisma.technology.findMany({
+    where: {
+      categoryId: Number(query.category.toString())
+    }
+  });
 
   await prisma.$disconnect();
 
